b797d5e66168f5ff7a9c09b0fb391043c90752cf
Maximilian Mader <mmader@uni-koblenz.de>
2018-10-31 20:24:43 +0100

    Add new bufferless fullscreen triangle app

---
+++ executables/fullscreen/main.cpp
@@ -0,0 +1,311 @@
+#include <iostream>
+#include <filesystem>
+
+#include <vulkan/vulkan.hpp>
+
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE // use Vulkans depth range [0, 1]
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#define VMA_IMPLEMENTATION
+#include "vma/vk_mem_alloc.h"
+
+#include "graphic/Context.h"
+#include "graphic/BaseApp.h"
+#include "graphic/Definitions.h"
+#include "userinput/Pilotview.h"
+#include "geometry/scene.h"
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+namespace vg
+{
+
+    class FSApp : public BaseApp
+    {
+    public:
+        FSApp()
+        {
+            createRenderPass();
+
+            createGraphicsPipeline();
+            createCommandPools();
+
+            createDepthResources();
+            createFramebuffers();
+
+            createPerFrameInformation();
+
+            createCommandBuffers();
+            createSyncObjects();
+        }
+
+        void createPerFrameInformation() override {}
+
+        // todo clarify what is here and what is in cleanupswapchain
+        ~FSApp()
+        {
+            m_context.getDevice().destroyImageView(m_depthImageView);
+            vmaDestroyImage(m_context.getAllocator(), m_depthImage.m_Image, m_depthImage.m_ImageAllocation);
+
+            for (int i = 0; i < m_context.max_frames_in_flight; i++)
+            {
+                m_context.getDevice().destroySemaphore(m_imageAvailableSemaphores.at(i));
+                m_context.getDevice().destroySemaphore(m_renderFinishedSemaphores.at(i));
+                m_context.getDevice().destroyFence(m_inFlightFences.at(i));
+            }
+
+            m_context.getDevice().destroyCommandPool(m_commandPool);
+            m_context.getDevice().destroyCommandPool(m_transferCommandPool);
+            m_context.getDevice().destroyCommandPool(m_computeCommandPool);
+
+            for (const auto framebuffer : m_swapChainFramebuffers)
+                m_context.getDevice().destroyFramebuffer(framebuffer);
+
+            m_context.getDevice().destroyPipeline(m_graphicsPipeline);
+            m_context.getDevice().destroyPipelineLayout(m_pipelineLayout);
+            m_context.getDevice().destroyRenderPass(m_renderpass);
+            // cleanup here
+        }
+
+
+        // base
+        void recreateSwapChain() override
+        {
+            int width = 0, height = 0;
+            while (width == 0 || height == 0)
+            {
+                glfwGetFramebufferSize(m_context.getWindow(), &width, &height);
+                glfwWaitEvents();
+            }
+
+            m_context.getDevice().waitIdle();
+
+            cleanUpSwapchain();
+
+            m_context.createSwapChain();
+            m_context.createImageViews();
+            createRenderPass();
+            createGraphicsPipeline();
+            createDepthResources();
+            createFramebuffers();
+            createCommandBuffers();
+        }
+
+        // base
+        void cleanUpSwapchain()
+        {
+            m_context.getDevice().destroyImageView(m_depthImageView);
+            vmaDestroyImage(m_context.getAllocator(), m_depthImage.m_Image, m_depthImage.m_ImageAllocation);
+
+
+            for (auto& scfb : m_swapChainFramebuffers)
+                m_context.getDevice().destroyFramebuffer(scfb);
+
+            m_context.getDevice().freeCommandBuffers(m_commandPool, m_commandBuffers);
+
+            m_context.getDevice().destroyPipeline(m_graphicsPipeline);
+            m_context.getDevice().destroyPipelineLayout(m_pipelineLayout);
+            m_context.getDevice().destroyRenderPass(m_renderpass);
+
+            for (auto& sciv : m_context.getSwapChainImageViews())
+                m_context.getDevice().destroyImageView(sciv);
+
+            m_context.getDevice().destroySwapchainKHR(m_context.getSwapChain());
+        }
+
+        void createGraphicsPipeline()
+        {
+            const auto vertShaderCode = Utility::readFile("fullscreen/shader.vert.spv");
+            const auto fragShaderCode = Utility::readFile("fullscreen/shader.frag.spv");
+
+            const auto vertShaderModule = m_context.createShaderModule(vertShaderCode);
+            const auto fragShaderModule = m_context.createShaderModule(fragShaderCode);
+
+            const vk::PipelineShaderStageCreateInfo vertShaderStageInfo({}, vk::ShaderStageFlagBits::eVertex, vertShaderModule, "main");
+            const vk::PipelineShaderStageCreateInfo fragShaderStageInfo({}, vk::ShaderStageFlagBits::eFragment, fragShaderModule, "main");
+
+            const vk::PipelineShaderStageCreateInfo shaderStages[] = { vertShaderStageInfo, fragShaderStageInfo };
+
+            vk::PipelineVertexInputStateCreateInfo vertexInputInfo({}, 0, nullptr, 0, nullptr);
+
+            vk::PipelineInputAssemblyStateCreateInfo inputAssembly({}, vk::PrimitiveTopology::eTriangleList, VK_FALSE);
+
+            vk::Viewport viewport(0.0f, 0.0f, static_cast<float>(m_context.getWidth()), static_cast<float>(m_context.getHeight()), 0.0f, 1.0f);
+
+            vk::Rect2D scissor({ 0, 0 }, m_context.getSwapChainExtent());
+
+            vk::PipelineViewportStateCreateInfo viewportState({}, 1, &viewport, 1, &scissor);
+
+            vk::PipelineRasterizationStateCreateInfo rasterizer({}, VK_FALSE, VK_FALSE, vk::PolygonMode::eFill, vk::CullModeFlagBits::eFront, vk::FrontFace::eCounterClockwise, VK_FALSE, 0, 0, 0, 1.0f);
+
+            vk::PipelineMultisampleStateCreateInfo mulitsampling({}, vk::SampleCountFlagBits::e1, VK_FALSE);
+
+            vk::PipelineDepthStencilStateCreateInfo depthStencil({}, VK_TRUE, VK_TRUE, vk::CompareOp::eLess);
+
+            vk::PipelineColorBlendAttachmentState colorBlendAttachment; // standard values for blending.
+            colorBlendAttachment.setColorWriteMask(vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG | vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA);
+            colorBlendAttachment.setBlendEnable(VK_FALSE);
+            // to enable blending, translate the following into hpp code: (and also use logic op copy in the struct below)
+            //colorBlendAttachment.blendEnable = VK_TRUE;
+            //colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
+            //colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+            //colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
+            //colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+            //colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+            //colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
+
+            vk::PipelineColorBlendStateCreateInfo colorBlending; // standard values for now
+            colorBlending.setLogicOpEnable(VK_FALSE);
+            colorBlending.setLogicOp(vk::LogicOp::eCopy);
+            colorBlending.attachmentCount = 1;
+            colorBlending.setPAttachments(&colorBlendAttachment);
+            colorBlending.setBlendConstants(std::array<float, 4>{0.0f, 0.0f, 0.0f, 0.0f});
+
+            //std::array<vk::DynamicState, 2> dynamicStates = { vk::DynamicState::eViewport, vk::DynamicState::eLineWidth };
+
+            //vk::PipelineDynamicStateCreateInfo dynamicState({}, dynamicStates.size(), dynamicStates.data());
+
+            vk::PipelineLayoutCreateInfo pipelineLayoutInfo;
+            pipelineLayoutInfo.setSetLayoutCount(0);
+
+            m_pipelineLayout = m_context.getDevice().createPipelineLayout(pipelineLayoutInfo);
+
+            vk::GraphicsPipelineCreateInfo pipelineInfo;
+            pipelineInfo.stageCount = 2;
+            pipelineInfo.pStages = shaderStages;
+            pipelineInfo.pVertexInputState = &vertexInputInfo;
+            pipelineInfo.pInputAssemblyState = &inputAssembly;
+            pipelineInfo.pViewportState = &viewportState;
+            pipelineInfo.pRasterizationState = &rasterizer;
+            pipelineInfo.pMultisampleState = &mulitsampling;
+            pipelineInfo.pDepthStencilState = &depthStencil;
+            pipelineInfo.pColorBlendState = &colorBlending;
+            pipelineInfo.pDynamicState = nullptr;// &dynamicState;
+
+            pipelineInfo.layout = m_pipelineLayout;
+            pipelineInfo.renderPass = m_renderpass;
+            pipelineInfo.subpass = 0;   // this is an index
+            // missing: pipeline derivation
+
+            m_graphicsPipeline = m_context.getDevice().createGraphicsPipeline(nullptr, pipelineInfo);
+
+            m_context.getDevice().destroyShaderModule(vertShaderModule);
+            m_context.getDevice().destroyShaderModule(fragShaderModule);
+        }
+
+        void createRenderPass()
+        {
+            vk::AttachmentDescription colorAttachment({}, m_context.getSwapChainImageFormat(),
+                vk::SampleCountFlagBits::e1,
+                vk::AttachmentLoadOp::eClear, vk::AttachmentStoreOp::eStore,            // load store op
+                vk::AttachmentLoadOp::eDontCare, vk::AttachmentStoreOp::eDontCare,      // stencil op
+                vk::ImageLayout::eUndefined, vk::ImageLayout::ePresentSrcKHR
+            );
+
+            vk::AttachmentReference colorAttachmentRef(0, vk::ImageLayout::eColorAttachmentOptimal);
+
+            vk::AttachmentDescription depthAttachment({}, vk::Format::eD32SfloatS8Uint,
+                vk::SampleCountFlagBits::e1,
+                vk::AttachmentLoadOp::eClear, vk::AttachmentStoreOp::eDontCare,
+                vk::AttachmentLoadOp::eDontCare, vk::AttachmentStoreOp::eDontCare,
+                vk::ImageLayout::eUndefined, vk::ImageLayout::eDepthStencilAttachmentOptimal
+            );
+
+            vk::AttachmentReference depthAttachmentRef(1, vk::ImageLayout::eDepthStencilAttachmentOptimal);
+
+            vk::SubpassDependency dependency(VK_SUBPASS_EXTERNAL, 0,
+                vk::PipelineStageFlagBits::eColorAttachmentOutput, vk::PipelineStageFlagBits::eColorAttachmentOutput,
+                {}, vk::AccessFlagBits::eColorAttachmentRead | vk::AccessFlagBits::eColorAttachmentWrite, vk::DependencyFlagBits::eByRegion);
+
+
+            vk::SubpassDescription subpass({}, vk::PipelineBindPoint::eGraphics,
+                0, nullptr,                 // input attachments (standard values)
+                1, &colorAttachmentRef,     // color attachments: layout (location = 0) out -> colorAttachmentRef is at index 0
+                nullptr,                    // no resolve attachment
+                &depthAttachmentRef);       // depth stencil attachment
+                                            // other attachment at standard values: Preserved
+
+            std::array<vk::AttachmentDescription, 2> attachments = { colorAttachment, depthAttachment };
+
+            vk::RenderPassCreateInfo renderpassInfo({}, static_cast<uint32_t>(attachments.size()), attachments.data(), 1, &subpass, 1, &dependency);
+
+            m_renderpass = m_context.getDevice().createRenderPass(renderpassInfo);
+
+        }
+
+        void createCommandBuffers()
+        {
+            vk::CommandBufferAllocateInfo cmdAllocInfo(m_commandPool, vk::CommandBufferLevel::ePrimary, static_cast<uint32_t>(m_swapChainFramebuffers.size()));
+
+            m_commandBuffers = m_context.getDevice().allocateCommandBuffers(cmdAllocInfo);
+
+            for (size_t i = 0; i < m_commandBuffers.size(); i++)
+            {
+                vk::CommandBufferBeginInfo beginInfo(vk::CommandBufferUsageFlagBits::eSimultaneousUse, nullptr);
+
+                // begin recording
+                m_commandBuffers.at(i).begin(beginInfo);
+
+                std::array<vk::ClearValue, 2> clearColors = { vk::ClearValue{std::array<float, 4>{ 0.1f, 0.1f, 0.1f, 1.0f }}, vk::ClearDepthStencilValue{1.0f, 0} };
+                vk::RenderPassBeginInfo renderpassInfo(m_renderpass, m_swapChainFramebuffers.at(i), { {0, 0}, m_context.getSwapChainExtent() }, static_cast<uint32_t>(clearColors.size()), clearColors.data());
+
+                /////////////////////////////
+                // actual commands start here
+                /////////////////////////////
+                m_commandBuffers.at(i).beginRenderPass(renderpassInfo, vk::SubpassContents::eInline);
+
+                m_commandBuffers.at(i).bindPipeline(vk::PipelineBindPoint::eGraphics, m_graphicsPipeline);
+
+                m_commandBuffers.at(i).draw(3, 1, 0, 0);
+
+                m_commandBuffers.at(i).endRenderPass();
+
+                // stop recording
+                m_commandBuffers.at(i).end();
+            }
+        }
+
+        void updatePerFrameInformation(uint32_t currentImage) override
+        {
+            auto cmdBuf = beginSingleTimeCommands(m_commandPool);
+
+            endSingleTimeCommands(cmdBuf, m_context.getGraphicsQueue(), m_commandPool);
+        }
+
+        void mainLoop()
+        {
+            while (!glfwWindowShouldClose(m_context.getWindow()))
+            {
+                glfwPollEvents();
+                drawFrame();
+            }
+
+            m_context.getDevice().waitIdle();
+        }
+
+    private:
+        vk::PipelineLayout m_pipelineLayout;
+        vk::Pipeline m_graphicsPipeline;
+    };
+}
+
+int main()
+{
+    vg::FSApp app;
+
+    try
+    {
+        app.mainLoop();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::terminate();
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
