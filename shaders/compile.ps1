0d366677be72bfdb520d9344a00d2e9811d249f4
Maximilian Mader <mmader@uni-koblenz.de>
2018-12-07 10:55:37 +0100

    Use vk sdk env variable in shader compile script

--- shaders/compile.ps1
+++ shaders/compile.ps1
@@ -3,11 +3,12 @@ Write-Output "Compiling shaders..."
 $types = @("*.vert", "*.frag", "*.tesc", "*.tese", "*.geom", "*.comp", "*.rgen", "*.rchit", "*.rmiss")
 $files = Get-Childitem -Include $types -Recurse -File
 
+
 foreach($file in $files)
 {
-    #$command = "C:\VulkanSDK\1.1.92.0\Bin\glslc.exe $file -o $file.spv -c"
-    #$command = "D:\mmader\vgraphics\shaders\glslangvalidator.exe -V $file -o $file.spv"
-    $command = "C:\VulkanSDK\1.1.92.1\Bin\glslangvalidator.exe -V $file -o $file.spv"
+    #$command = "$Env:VK_SDK_PATH\Bin\glslc.exe $file -o $file.spv -c"
+    #$command = ".\glslangvalidator.exe -V $file -o $file.spv"
+    $command = "$Env:VK_SDK_PATH\Bin\glslangvalidator.exe -V $file -o $file.spv"
 
     #Write-Output $command
     Invoke-Expression $command
