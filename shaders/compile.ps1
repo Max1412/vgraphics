f4514693b39d0250f9ac811f384bdefc3303d539
Maximilian Mader <mmader@uni-koblenz.de>
2018-12-22 20:22:33 +0100

    Delete old stuff

--- shaders/compile.ps1
+++ shaders/compile.ps1
@@ -7,7 +7,6 @@ $files = Get-Childitem -Include $types -Recurse -File
 foreach($file in $files)
 {
     #$command = "$Env:VK_SDK_PATH\Bin\glslc.exe $file -o $file.spv -c"
-    #$command = ".\glslangvalidator.exe -V $file -o $file.spv"
     $command = "$Env:VK_SDK_PATH\Bin\glslangvalidator.exe -V $file -o $file.spv"
 
     #Write-Output $command
