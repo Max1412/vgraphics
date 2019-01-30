f5367908ad39bc573a8ff58899b47bb8038227bf
Maximilian Mader <mmader@uni-koblenz.de>
2019-01-30 18:24:37 +0100

    Use correct path corresponding to binary path

--- libraries/graphic/Definitions.h
+++ libraries/graphic/Definitions.h
@@ -11,8 +11,8 @@ namespace vg
     const std::vector<const char*> g_validationLayers = { "VK_LAYER_LUNARG_standard_validation", "VK_LAYER_LUNARG_assistant_layer" };
     const std::vector<const char*> g_deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME, "VK_KHR_shader_draw_parameters" };
 
-    const auto g_resourcesPath = std::filesystem::current_path().parent_path().append("resources");
-    const auto g_shaderPath = std::filesystem::current_path().parent_path().append("shaders");
+    const auto g_resourcesPath = std::filesystem::current_path().parent_path().parent_path().append("resources");
+    const auto g_shaderPath = std::filesystem::current_path().parent_path().parent_path().append("shaders");
 
 #ifdef NDEBUG
     constexpr bool enableValidationLayers = false;
