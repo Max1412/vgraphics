0cfeebe6a2d7ff641c9fd3f2743c2abac53f1693
Maximilian Mader <mmader@uni-koblenz.de>
2019-02-26 16:50:51 +0100

    Add folder and compiler option to shader compiler script

--- shaders/compile.ps1
+++ shaders/compile.ps1
@@ -1,20 +1,38 @@
-Write-Output "Compiling shaders..."
+param(
+    [string]$Folder = "",
+    [string]$Compiler = "glslc"
+)
+$startoutput = "Compiling shaders using " + $Compiler
+Write-Output $startoutput
 
 $types = @("*.vert", "*.frag", "*.tesc", "*.tese", "*.geom", "*.comp", "*.rgen", "*.rchit", "*.rmiss", "*.rahit")
-$files = Get-Childitem -Include $types -Recurse -File
+$files = Get-Childitem $Folder -Include $types -Recurse -File
 
 
 foreach($file in $files)
 {
-    $command = "$Env:VK_SDK_PATH\Bin\glslc.exe $file -o $file.spv -c -I include"
-    #$command = "$Env:VK_SDK_PATH\Bin\glslangvalidator.exe -V $file -o $file.spv"
+    if($Compiler -eq "glslc")
+    {
+        $command = "$Env:VK_SDK_PATH\Bin\glslc.exe $file -o $file.spv -c -I include"
 
-    #Write-Output $command
         $name = [System.IO.Path]::GetFileName($file)
         $end = [System.IO.Path]::GetDirectoryName($file).split("\")
         $lastDirName = $end[$end.Count - 1]
-    Write-Output $lastDirName\$name
-    Invoke-Expression $command
+        Write-Output $lastDirName\$name
+        Invoke-Expression $command
+    }
+    elseif($Compiler -eq "glslangvalidator")
+    {
+        $command = "$Env:VK_SDK_PATH\Bin\glslangvalidator.exe -V $file -o $file.spv"
+        Invoke-Expression $command
+    }
+    else
+    {
+        Write-Output "Please choose glslc or glslangvalidator as compiler"
+        break
+    }
 }
+
 $date = Get-Date -Format g
-Write-Output "Done:" $date 
+$finishOutput = "Done: " + $date
+Write-Output $finishOutput 
